db4fe85021841924d436ad258ea1238fa78d60cd
Falk Scheerschmidt <falk@staffbase.com>
2021-04-13 22:11:51 +0200

    add command to copy a pad

---
+++ cmd/copy-pad.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/systemli/etherpad-toolkit/pkg"
+)
+
+var (
+	forceCopy bool
+
+	copyPadCmd = &cobra.Command{
+		Use:   "copy-pad [sourceID] [destinationID]",
+		Short: "Copies a single Pad",
+		Long:  "The command copies a pad with full history and chat.. If force is true and the destination pad exists, it will be overwritten.",
+		Run:   runCopy,
+	}
+)
+
+func init() {
+	copyPadCmd.LocalFlags().BoolVar(&forceCopy, "force", false, "If set and the destination pad exists, it will be overwritten.")
+
+	rootCmd.AddCommand(copyPadCmd)
+}
+
+func runCopy(cmd *cobra.Command, args []string) {
+	if len(args) != 2 {
+		fmt.Println(cmd.UsageString())
+		return
+	}
+
+	etherpad := pkg.NewEtherpadClient(etherpadUrl, etherpadApiKey)
+	sourceID := args[0]
+	destinationID := args[1]
+
+	err := etherpad.CopyPad(sourceID, destinationID, forceCopy)
+	if err != nil {
+		log.WithError(err).WithFields(log.Fields{"sourceID": sourceID, "destinationID": destinationID}).Error("error while copy pad")
+	} else {
+		log.WithFields(log.Fields{"sourceID": sourceID, "destinationID": destinationID}).Info("pad successfully copied")
+	}
+}
